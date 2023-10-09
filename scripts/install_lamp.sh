4ef5179696ea813d055cb3784533c8e2c07d8c9c
alexgcelia <alexgimenez.elo@gmail.com>
2023-10-09 12:12:35 +0000

    Modificamos el lamp y añadimos las tools y el conf

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -10,4 +10,23 @@ set -x
 #apt upgrade -y
 
 #Instalamos el servidor web Apache
-apt install apache2 -y
+sudo apt install apache2 -y
+
+#Instalamos el gestor de bases de datos MySQL
+sudo apt install mysql-server -y
+
+#Instalamos PHP
+apt install php libapache2-mod-php php-mysql -y
+
+#Copiar el archivo de configuración de Apache
+cp ../conf/000-default.conf /etc/apache2/sites-available
+
+#Reiniciamos el servicio de Apache
+systemctl restart apache2
+
+#Copiamos el archivo de prueba de PHP
+cp ../php/index.php /var/www/html
+
+#Modificamos el propietario y el grupo del directorio /var/www/html
+chrow -R www.data:www-data /var/www/html
+
